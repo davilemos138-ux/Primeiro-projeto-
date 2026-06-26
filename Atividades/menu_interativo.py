f8c285ddb03bbe6df8cd973607658af0d2890a84
davilemos138-ux <davilemos138@gmail.com>
2026-06-25 21:17:52 -0300

    envio de exercícios

---
+++ Atividades/menu_interativo.py
@@ -0,0 +1,24 @@
+
+
+while True:
+   opcao = print("""1 - Somar
+2 - Subtrair
+3 - Multiplicar
+4 - Dividir
+5 - Sair
+Digite uma das opções: """)
+
+   if opcao == 1:
+       print("Somar")
+   elif opcao == 2:
+       print("Subtrair")
+   elif opcao == 3:
+       print("Multiplicar")
+   elif opcao == 4:
+       print("Dividir")
+   elif opcao == 5:
+       print("Sair")
+
+
+
+
---
+++ Atividades/somador_at_zero.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um número: "))
+soma = 0
+
+while num != 0:
+    soma += num
+    num = int(input("Digite um número"))
+print(f"Total da soma:{soma}")
+
+
